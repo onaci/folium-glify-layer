d94935e1fe04cb417f19c46b081d3580f17666a9
Dan Wild <danwild@y7mail.com>
2021-05-11 16:40:45 +1000

    bumpversion

--- folium_glify_layer/__init__.py
+++ folium_glify_layer/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from folium.elements import JSCSSMixin
 from folium.map import Layer
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="folium-glify-layer",
-    version="0.2.0",
+    version="0.3.0",
     description="Folium plugin to provide fast webgl rendering for GeoJSON FeatureCollections.",
     long_description=README,
     long_description_content_type="text/markdown",
